e914bf6605646a7fe6df831ccc53ece15c806ac7
elielMengue <156508960+elielMengue@users.noreply.github.com>
2025-08-28 13:27:29 +0000

    implementation

--- cakeCalculator.cpp
+++ cakeCalculator.cpp
@@ -1,2 +1,14 @@
-//
+// 1. Cake calculator function
+
+#include <tuple>
+// Returns a triplet (cakes, flour_remainder, sugar_remainder)
+std::tuple<int, int, int> cake_calculator(int flour, int sugar) {
+ int cakes = 0;
+ while  (flour >= 100 && sugar >= 50) {
+ 	flour -= 100;
+ 	sugar -= 50;
+ 	++cakes;
+ }
+ return {cakes, flour, sugar};
+}
 
